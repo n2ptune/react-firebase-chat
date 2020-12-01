77ffc0e61ebc357c7d6e3c0593a10d4d5684f8bc
n2ptune <n2ptune@outlook.com>
2020-12-01 13:59:40 +0900

    feat: global styles, header style

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,53 @@
 import styled from 'styled-components'
-import Box from '@material-ui/core/Box'
+import { Box, Button, CircularProgress, makeStyles } from '@material-ui/core'
+import { grey, blue } from '@material-ui/core/colors'
+import { useState } from 'react'
+
+const useStyles = makeStyles(theme => {
+  return {
+    loadingButton: {
+      color: blue['700'],
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      marginTop: -8,
+      marginLeft: -6
+    }
+  }
+})
 
 const StyledHeader = styled(Box)`
-  display: flex;
+  border-bottom: 1px solid ${grey['300']};
+  margin-bottom: 1rem;
+`
+
+const ButtonBox = styled(Box)`
+  border-radius: 5px;
+  border: 1px solid ${grey['300']};
 `
 
 export default function Header() {
-  return <StyledHeader p={1}>Header</StyledHeader>
+  const classes = useStyles()
+  const [isLoading, setIsLoading] = useState(true)
+
+  return (
+    <StyledHeader
+      alignItems="center"
+      component="header"
+      display="flex"
+      justifyContent="space-between"
+      p={2}
+      width="100%"
+    >
+      <Box fontSize={18} fontWeight="bold">
+        모두챗
+      </Box>
+      <ButtonBox>
+        <Button size="small" disabled={isLoading}>
+          로그인
+          <CircularProgress size={16} className={classes.loadingButton} />
+        </Button>
+      </ButtonBox>
+    </StyledHeader>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { init as fireabseInit } from 'api/firebase'
+import 'styles/global.css'
 
 fireabseInit()
 
