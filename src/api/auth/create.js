83cd50fd0c88c05af078720bce0cd7ac35857d25
n2ptune <n2ptune@outlook.com>
2020-12-05 13:35:52 +0900

    feat: create account methods

--- src/api/auth/create.js
+++ src/api/auth/create.js
@@ -1,1 +1,10 @@
 import { auth } from 'api/firebase'
+
+function createAccount(email, password) {
+  return auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(user => user)
+    .catch(error => error)
+}
+
+export { createAccount }
