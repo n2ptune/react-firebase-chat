5e2a48b61bfb38c26c2cfcd0490e8e14dfa64514
n2ptune <n2ptune@outlook.com>
2020-12-07 13:31:10 +0900

    fix: handle login error

--- src/api/auth/login.js
+++ src/api/auth/login.js
@@ -3,3 +3,18 @@ import firebase from 'api/firebase'
 export async function email(email, password) {
   return await firebase.auth().signInWithEmailAndPassword(email, password)
 }
+
+export function authErrorHandler(errorCode) {
+  switch (errorCode) {
+    case 'auth/invalid-email':
+      return '유효하지 않은 이메일입니다.'
+    case 'auth/user-disabled':
+      return '활성화되지 않은 계정입니다.'
+    case 'auth/user-not-found':
+      return '계정을 찾을 수 없습니다.'
+    case 'auth/wrong-password':
+      return '이메일 혹은 비밀번호가 틀렸습니다.'
+    default:
+      return '알 수 없는 오류'
+  }
+}
--- src/components/auth/Form.js
+++ src/components/auth/Form.js
@@ -1,6 +1,13 @@
-import React from 'react'
-import { TextField, Button, ButtonGroup, makeStyles } from '@material-ui/core'
+import React, { useState } from 'react'
+import {
+  Button,
+  ButtonGroup,
+  makeStyles,
+  TextField,
+  Typography
+} from '@material-ui/core'
 import { login } from 'api/auth'
+import { authErrorHandler } from 'api/auth/login'
 
 const useStyles = makeStyles(theme => {
   return {
@@ -13,14 +20,22 @@ const useStyles = makeStyles(theme => {
     buttonCommon: {
       width: '100%',
       fontWeight: theme.typography.fontWeightBold
+    },
+    errorMessage: {
+      fontSize: theme.typography.pxToRem(12),
+      textDecoration: 'underline',
+      marginBottom: theme.spacing(1),
+      color: theme.palette.error.main
     }
   }
 })
 
 export default function LoginForm() {
   const classes = useStyles()
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
 
   function handleEmailChange(event) {
     setEmail(event.target.value)
@@ -31,13 +46,24 @@ export default function LoginForm() {
   }
 
   async function handleLogin() {
-    const user = await login.email(email, password)
+    setIsLoading(true)
 
-    console.log(user)
+    try {
+      const user = await login.email(email, password)
+      console.log(user)
+      setErrorMessage('')
+    } catch (error) {
+      setErrorMessage(authErrorHandler(error.code))
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return (
     <form className={classes.form} noValidate autoComplete="off">
+      {errorMessage && (
+        <Typography className={classes.errorMessage}>{errorMessage}</Typography>
+      )}
       <TextField
         fullWidth
         label="이메일"
@@ -45,6 +71,7 @@ export default function LoginForm() {
         onChange={handleEmailChange}
         value={email}
         variant="outlined"
+        disabled={isLoading}
       />
       <TextField
         fullWidth
@@ -54,10 +81,10 @@ export default function LoginForm() {
         type="password"
         value={password}
         variant="outlined"
+        disabled={isLoading}
       />
       <ButtonGroup variant="outlined" className={classes.buttonGroup}>
         <Button
-          disableRipple
           onClick={handleLogin}
           color="primary"
           className={classes.buttonCommon}
